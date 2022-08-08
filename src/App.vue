8b9d5636f4cdba0fd1d315c8196e87de2cdbbe0f
Deniz <denizderezzed@gmail.com>
2022-08-08 19:58:22 +0200

    proxy for lotohelden

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>blub</template>
+
+<script setup lang="ts">
+import axios from 'axios'
+
+axios
+  .get('/api?query={draw}')
+  .then((res) => {
+    console.log(res.data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
